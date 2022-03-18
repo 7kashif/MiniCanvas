27090cf54d2dff09e484d47bcfa62fd9f5432feb
Kashif_M <khanzada.kashif2001@gmail.com>
2022-03-18 23:10:14 +0500

    canvas with xml

--- app/src/main/java/com/kashif/minicanvas/MainActivity.kt
+++ app/src/main/java/com/kashif/minicanvas/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.kashif.minicanvas
 
 import android.os.Bundle
+import android.view.SurfaceHolder
 import androidx.appcompat.app.AppCompatActivity
+import com.kashif.minicanvas.databinding.ActivityMainBinding
+import javax.security.auth.callback.Callback
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         val myCanvasView = MyCanvasView(this)
-        myCanvasView.fitsSystemWindows = true
         myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
-        setContentView(myCanvasView)
+        setContentView(binding.root)
     }
+
 }
